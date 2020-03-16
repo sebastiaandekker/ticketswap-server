f573b4ebf4c918bee4b577d0cf783fd3b94f3076
Sebastiaan Dekker <sebastiaan@Sebastiaans-MacBook-Pro.local>
2020-03-16 15:10:50 +0100

    Add route: update Ticket

--- ticket/router.js
+++ ticket/router.js
@@ -32,4 +32,19 @@ router.get("/ticket/:ticketId", (req, res, next) => {
     .catch(next);
 });
 
+router.put("/ticket/:ticketId", (req, res, next) =>
+  Ticket.findByPk(req.params.ticketId)
+    .then(ticket => {
+      if (!ticket) {
+        res.status(404).end();
+      } else {
+        return ticket.update(req.body);
+      }
+    })
+    .then(ticket => {
+      res.send(ticket);
+    })
+    .catch(next)
+);
+
 module.exports = router;
